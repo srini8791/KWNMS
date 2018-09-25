34d7e7044d948f3c085a8628b423fdad9be6c51b
srinivas <srinivas@hitsquadtech.com>
2018-09-25 16:52:01 +0530

    INV-01 Added Bandwidth enum and saving it to DB

---
+++ opennms/opennms-model/src/main/java/org/opennms/netmgt/model/Bandwidth.java
@@ -0,0 +1,90 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2007-2014 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2014 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.netmgt.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * OpenNMS Radio Mode.
+ */
+public enum Bandwidth {
+
+    BANDWIDTH_20MHZ(1, "Bw20MHz"),
+    BANDWIDTH_40MHZ(2, "Bw40MHz"),
+    BANDWIDTH_80MHZ(3, "Bw80MHz"),
+    BANDWIDTH_UNKNOWN(4,"Unknown");
+
+    private static final Map<Integer, Bandwidth> m_idMap;
+
+    private int m_id;
+    private String m_label;
+
+    static {
+        m_idMap = new HashMap<>(values().length);
+        for (final Bandwidth bandwidth : values()) {
+            m_idMap.put(bandwidth.getId(), bandwidth);
+        }
+    }
+
+    Bandwidth(int id, String label) {
+        m_id = id;
+        m_label = label;
+    }
+
+    public int getId() {
+        return m_id;
+    }
+
+    public String getLabel() {
+        return m_label;
+    }
+
+    public static Bandwidth get(final int id) throws IllegalArgumentException {
+        if (m_idMap.containsKey(id)) {
+            return m_idMap.get(id);
+        } else {
+            throw new IllegalArgumentException("Cannot create Bandwidth from unknown ID " + id);
+        }
+    }
+
+    public static Bandwidth get(final String label) {
+        for (final Integer key : m_idMap.keySet()) {
+            if (m_idMap.get(key).getLabel().equalsIgnoreCase(label)) {
+                return m_idMap.get(key);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return m_label;
+    }
+}
