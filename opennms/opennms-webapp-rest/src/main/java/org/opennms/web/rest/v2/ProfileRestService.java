1134718678e8b45668d844fe1c59ff83ada65c81
nagaranjan <nagaranjan@hitsquadtech.com>
2018-10-30 00:23:01 +0530

    Fixed issue with saving the labels of enum objects

--- opennms/opennms-webapp-rest/src/main/java/org/opennms/web/rest/v2/ProfileRestService.java
+++ opennms/opennms-webapp-rest/src/main/java/org/opennms/web/rest/v2/ProfileRestService.java
@@ -179,14 +179,19 @@ public class ProfileRestService extends AbstractDaoRestService<OnmsProfile, Sear
 
     private List<String> buildPropsToWrite(OnmsProfile profile) {
         List<String> props = new ArrayList<>();
+        String profileSsid = profile.getSsid();
+        OpMode profileOpMode = profile.getOpMode();
+        Bandwidth profileBandwidth = profile.getBandwidth();
+        Integer profileChannel = profile.getChannel();
+
         for (String key : PROFILE_PROPS_MAP.keySet()) {
             String propValue = "";
-            if (key.equals("wireless.@wifi-iface[1].ssid") && profile.getSsid() != null) {
-                propValue =  "'" + profile.getSsid() + "'";
-            } else if (key.equals("wireless.wifi1.hwmode") && profile.getOpMode() != null) {
-                propValue = "'" + String.valueOf(profile.getOpMode().getId()) + "'";
-            } else if (key.equals("wireless.wifi1.htmode") && profile.getBandwidth() != null) {
-                propValue = "'" + String.valueOf(profile.getBandwidth().getId()) + "'";
+            if (key.equals("wireless.@wifi-iface[1].ssid") && profileSsid != null) {
+                propValue =  "'" + profileSsid + "'";
+            } else if (key.equals("wireless.wifi1.hwmode") && profileOpMode != null && profileOpMode != OpMode.MODE_UNKNOWN) {
+                propValue = "'" + String.valueOf(profileOpMode.getLabel()) + "'";
+            } else if (key.equals("wireless.wifi1.htmode") && profileBandwidth != null && profileBandwidth != Bandwidth.BANDWIDTH_UNKNOWN) {
+                propValue = "'HT" + profileBandwidth.getLabel().replaceAll("MHz", "") + "'";
             } else if (key.equals("wireless.wifi1.channel") && profile.getChannel() != null) {
                 propValue = "'" + String.valueOf(profile.getChannel()) + "'";
             }
