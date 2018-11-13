8e4507096f977f73dc51f4f505b1508c6b51651a
nagaranjan <nagaranjan@hitsquadtech.com>
2018-11-13 17:53:25 +0530

    Added inventory report in reports page of new UI

--- opennms/opennms-webapp/src/main/webapp/app/modules/dashboard/controllers/reports.js
+++ opennms/opennms-webapp/src/main/webapp/app/modules/dashboard/controllers/reports.js
@@ -3,6 +3,44 @@
 dashboard.controller("ReportsController", ['$rootScope', '$scope', '$http', '$state', '$location', 'dashboardService', 'Flash',
   function ($rootScope, $scope, $http, $state, $location, dashboardService, Flash) {
     var vm = this;
+
+    $scope.limit = 10;
+    $scope.vm.nodes = [];
+    $scope.vm.totalNodesCount = 0;
+    $scope.vm.activeNodesCount = 0;
+
+    $scope.init = function() {
+      $scope.loadNodes();
+      $scope.loadActiveNodesCount();
+    }
+
+    $scope.loadNodes = function() {
+      var config = {
+        params: {
+          'limit': $scope.limit
+        }
+      };
+
+      $http.get('api/v2/nodes', config)
+        .then(function(response) {
+          if (response.data) {
+            $scope.vm.totalNodesCount = response.data.totalCount;
+            $scope.vm.nodes = response.data.node;
+          }
+        }
+      );
+    }
+
+    $scope.loadActiveNodesCount = function() {
+      $http.get('api/v2/nodes/active/count')
+        .then(function(response) {
+          if (response.data) {
+            $scope.vm.activeNodesCount = response.data;
+          }
+        }
+      );
+    }
+
   }
 
 ]);
