f782033ada0f0ea05961db5d1c57bf7ef279b64c
srinivas <srinivas@hitsquadtech.com>
2018-11-16 16:25:55 +0530

    TOPO-01 Added implementation for showing markers on the map

--- opennms/opennms-webapp/src/main/webapp/app/modules/dashboard/controllers/management.js
+++ opennms/opennms-webapp/src/main/webapp/app/modules/dashboard/controllers/management.js
@@ -9,10 +9,20 @@ dashboard.controller("ManagementController", ['$rootScope', '$scope', '$http', '
     $scope.vm.totalNodesCount = 0;
     $scope.vm.activeNodesCount = 0;
 
+    // variables related to map
+    // start
+    $scope.map = null;
+    $scope.markers = [];
+    var infoWindow = new google.maps.InfoWindow();
+    // end
+
+
+
     $scope.init = function() {
       $scope.buildTree();
       $scope.loadNodes("");
       $scope.loadActiveNodesCount();
+      $scope.prepareMap();
     }
 
     $scope.loadTreeRegions = function() {
@@ -46,6 +56,59 @@ dashboard.controller("ManagementController", ['$rootScope', '$scope', '$http', '
     }
 
 
+
+    // functions related to Map start
+    $scope.prepareMap = function() {
+        var mapOptions = {
+            zoom : 9,
+            center : new google.maps.LatLng('17.4938439','78.428827'),
+            mapTypeId : google.maps.MapTypeId.ROADMAP
+        }
+        $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    }
+
+    $scope.showNodesOnMap = function() {
+        if ($scope.vm.totalNodesCount > 0) {
+            for(var i = 0; i<$scope.vm.totalNodesCount;i++) {
+                $scope.addNodeOnMap($scope.vm.nodes[i]);
+            }
+        }
+    }
+
+    $scope.addNodeOnMap = function(nodeObj) {
+            if (nodeObj != null) {
+                var latitude = nodeObj.assetRecord.latitude;
+                var longitude = nodeObj.assetRecord.longitude;
+                if (latitude == 0) {
+                    latitude = 0.0;
+                }
+                if (longitude == 0) {
+                    longitude = 0.0;
+                }
+                var myLatlng = new google.maps.LatLng(latitude, longitude);
+                var marker = new google.maps.Marker({
+                    map : $scope.map,
+                    position: myLatlng,
+                    title: nodeObj.primaryIP,
+                    ssid:nodeObj.ssid
+                });
+                google.maps.event.addListener(marker,'click',function() {
+                    infoWindow.setContent( '<b>' + marker.title + '</b>'
+                        +'<p><b>Address:</b>'
+                        + marker.title
+                        + '<br/><b>SSID:</b>' + marker.ssid
+                        + '</p>'
+                        );
+                    infoWindow.open($scope.map,marker);
+
+                });
+                //marker.setMap($scope.map);
+                $scope.markers.push(marker);
+            }
+        }
+
+        // functions related to Map end
+
     $scope.loadNodes = function(location) {
       var config = {
         params: {
@@ -57,11 +120,13 @@ dashboard.controller("ManagementController", ['$rootScope', '$scope', '$http', '
       if (location && location != '') {
         url = 'api/v2/treeview/' + location + '/nodes';
       }
-      $http.get(url, config)
+      return $http.get(url, config)
         .then(function(response) {
           if (response.data) {
-            $scope.vm.totalNodesCount = response.data.totalCount;
-            $scope.vm.nodes = response.data.node;
+            /*$scope.vm.totalNodesCount = response.data.length;
+            $scope.vm.nodes = response.data;*/
+            return response.data;
+
           }
         }
       );
@@ -87,16 +152,21 @@ dashboard.controller("ManagementController", ['$rootScope', '$scope', '$http', '
     $scope.$watch('mytree.currentNode', function( newObj, oldObj ) {
       if( $scope.mytree && angular.isObject($scope.mytree.currentNode) ) {
         var nodeType = $scope.mytree.currentNode.data.type;
-        if (nodeType === 'root') {
+        if (nodeType === 'root') { // Global
           $scope.loadTreeRegions().then(function(data) {
             $scope.mytree.currentNode.children = data;
           });
-        } else if (nodeType === 'region') {
+        } else if (nodeType === 'region') { // area location -- for eg: geometrical boundary
           $scope.loadTreeLocations($scope.mytree.currentNode.data.id).then(function(data) {
             $scope.mytree.currentNode.children = data;
           });
-        } else if (nodeType === 'location') {
-          $scope.loadNodes($scope.mytree.currentNode.data.id);
+        } else if (nodeType === 'location') { // location -- facility or locations under region
+          $scope.loadNodes($scope.mytree.currentNode.data.id).then(function(data) {
+            $scope.vm.nodes = data;
+            $scope.vm.totalNodesCount = data.length;
+            $scope.showNodesOnMap();
+          });
+
         }
       }
     }, false);
