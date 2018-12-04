1ef4a974648b224b9bfe53d7797bdeee79e35ae6
nagaranjan <nagaranjan@hitsquadtech.com>
2018-12-04 16:49:07 +0530

    Cleaned-up settings js to have only region related content

--- opennms/opennms-webapp/src/main/webapp/app/modules/dashboard/controllers/settings.js
+++ opennms/opennms-webapp/src/main/webapp/app/modules/dashboard/controllers/settings.js
@@ -15,34 +15,18 @@ dashboard.controller("SettingsController", ['$rootScope', '$scope', '$mdDialog',
       };
     };
 
-    $scope.profiles = [];
-    $scope.newProfile = {
-      'name': '',
-      'ssid': '',
-      'opMode': '',
-      'bandwidth': '',
-      'channel': '',
-      'minimumFirmware': ''
-    };
-    $scope.nodeProfile = {};
-
     $scope.regions = [];
     $scope.newRegion = {
       'name': ''
     };
 
-    $scope.nodes = [];
-    $scope.profileToApply = '';
-    $scope.nodesToApply = [];
-
     $scope.init = function() {
       $scope.loadRegions();
-      $scope.loadProfiles();
-      $scope.loadNodes();
     };
 
     $scope.loadRegions = function() {
-      $http.get('api/v2/regions')
+      var paramObj = { 'params': {'limit': 0} };
+      $http.get('api/v2/regions', paramObj)
         .then(function(response) {
           if (response.data) {
             $scope.regions = response.data.region;
@@ -51,79 +35,6 @@ dashboard.controller("SettingsController", ['$rootScope', '$scope', '$mdDialog',
         }
       );
     }
-
-    $scope.loadProfiles = function() {
-      $http.get('api/v2/profiles')
-        .success(function(result) {
-          $scope.profiles = result.profile;
-        })
-        .error(function(msg) {
-          $scope.showAlert('error', msg);
-        });
-    };
-
-    $scope.retrieveProfile = function(nodeId) {
-      var configObj = {
-        'params': {
-          'nodeId': nodeId
-        }
-      };
-
-      $http.get('api/v2/profiles/retrieveProfile', configObj)
-        .then(function(response) {
-          if (response.status === 200) {
-            $scope.showAlert('success', 'Successfully retrieved profile to server.');
-          }
-        })
-        .catch(function(response) {
-          if (response.data['errorMessage']) {
-            $scope.showAlert('error', response.data['errorMessage']);
-          } else {
-            $scope.showAlert('error', 'Error retrieving profile.');
-          }
-
-        });
-    };
-
-    $scope.populateFormWithProfile = function(nodeId) {
-      var configObj = {
-        'params': {
-          'nodeId': nodeId
-        }
-      };
-
-      $http.get('api/v2/profiles/extractProfile', configObj)
-        .then(function(response) {
-          if (response.data) {
-            $scope.newProfile.ssid = response.data['ssid'];
-            $scope.newProfile.opMode = response.data['opModeId'];
-            $scope.newProfile.bandwidth = response.data['bandwidthId'];
-            $scope.newProfile.channel = response.data['channel'];
-          }
-        })
-        .catch(function(response) {
-          if (response.data['errorMessage']) {
-            $scope.showAlert('error', response.data['errorMessage']);
-          } else {
-            $scope.showAlert('error', 'Error retrieving profile.');
-          }
-        });
-    };
-
-    $scope.saveProfile = function() {
-      $http({
-        method: 'POST',
-        url: 'api/v2/profiles',
-        headers: {'Content-Type': 'application/json'},
-        data: $scope.newProfile
-      }).success(function() {
-        $scope.showAlert('success', 'The profile has been created successfully.');
-        $scope.newProfile = {};
-        $scope.loadProfiles();
-      }).error(function(msg) {
-        $scope.showAlert('error', 'Error creating the profile: ' + msg);
-      });
-    };
 
     $scope.saveRegion = function() {
       $http({
@@ -139,37 +50,6 @@ dashboard.controller("SettingsController", ['$rootScope', '$scope', '$mdDialog',
         $scope.showAlert('error', 'Error creating the region: ' + msg);
       });
     };
-
-    $scope.loadNodes = function() {
-      $http.get('api/v2/nodes')
-        .success(function(result) {
-          $scope.nodes = result.node;
-        })
-        .error(function(msg) {
-          $scope.showAlert('error', msg);
-        });
-    };
-
-    $scope.apply = function() {
-      if ($scope.profileToApply === '' || $scope.nodesToApply.length === 0) {
-        $scope.showAlert('error', 'Select a profile and nodes to apply');
-        return;
-      }
-
-      $http({
-        method: 'POST',
-        url: 'api/v2/profiles/' + $scope.profileToApply + '/applyToNodes',
-        headers: {'Content-Type': 'application/json'},
-        data: $scope.nodesToApply
-      }).success(function() {
-        $scope.showAlert('success', 'The profile has been initiated to apply for the selected nodes.');
-        $scope.profileToApply = '';
-        $scope.nodesToApply = [];
-      }).error(function(msg) {
-        $scope.showAlert('error', 'Cannot apply the profile: ' + msg);
-      });
-    };
-
 
     $scope.showAlert = function(msgType, msg, ev) {
       $mdDialog.show(
